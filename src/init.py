2e193cbad8de791d7aa76890229931d3cfd385c2
seanolly <connolly.s@husky.neu.edu>
2018-02-03 22:37:10 -0800

    predictions working

--- src/init.py
+++ src/init.py
@@ -14,10 +14,36 @@ twitter = Twython(consumer_key,
                   access_token,
                   access_token_secret)
 
+
+import stopwords as stopwords
+
+from authentication import (consumer_key, consumer_secret, access_token, access_token_secret)
+import numpy as np
+from twython import Twython
+from sklearn.naive_bayes import MultinomialNB
+
+from preprocessor import preprocessNormalTweets, preprocessJoke, jokeData, normalTweetData, labelData
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_selection import SelectPercentile, f_classif
+import nltk.corpus
+from nltk.stem import snowball
+from sklearn.naive_bayes import GaussianNB
+#initialize twitter account credentials
+twitter = Twython(consumer_key,
+                  consumer_secret,
+                  access_token,
+                  access_token_secret)
+
+
+
 def initFeatures(limit):
     ### text vectorization--go from strings to lists of numbers
-    vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
-                                 stop_words='english')
+    # words to exclude
+    exclusion = stopwords.get_stopwords('english')
+
+    # vectorizer
+    vectorizer = CountVectorizer(stop_words=exclusion)
+
     jokes = preprocessJoke(limit, jokeData)
     jokeLabels = labelData(jokes, 1) #1 is joke
     print ("Joke Labels length:", len(jokeLabels))
@@ -33,30 +59,24 @@ def initFeatures(limit):
     #concat features
     training_features = jokes + tweets
     print ("training feature length:", len(training_features))
-    transformedFeatures = vectorizer.fit_transform(training_features)
+    transformedFeatures = vectorizer.fit_transform(training_features).toarray()
 
-
-    #feature selection
-    selection = SelectPercentile(f_classif, percentile=1)
-    selection.fit(transformedFeatures, training_labels)
-    finalTrainingFeatures = selection.transform(transformedFeatures).toarray()
-
-    return finalTrainingFeatures, training_labels,vectorizer
+    return transformedFeatures, training_labels, vectorizer
 
 def makePrediction(vectorizer,features_train,labels_train, tweets):
     classifier = GaussianNB()
     classifier.fit(features_train, labels_train)
-    transformedTweets = vectorizer.transform(tweets)
+    transformedTweets = vectorizer.transform(tweets).toarray()
     print ("Transformed tweet: ", transformedTweets)
-    prediction = classifier.predict((transformedTweets).toarray())
+    prediction = classifier.predict((transformedTweets))
     return prediction
 
 
 
 def run(data_limit):
     #get features, labels
-    features_train, labels_train,vectorizer = initFeatures(1000)
-    prediction = makePrediction(vectorizer, features_train, labels_train, np.asarray(["Knock Knock who's there?"]))
+    features_train, labels_train,vectorizer = initFeatures(2040)
+    prediction = makePrediction(vectorizer, features_train, labels_train, ["The governor of England"])
     print prediction
 
 run(1000)
