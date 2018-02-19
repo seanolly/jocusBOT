db999c23be4052097f008271032de4c67abe194f
Sean Connolly <connolly.s@husky.neu.edu>
2018-02-18 19:54:19 -0800

    Delete authentication.py

--- src/authentication.py
+++
@@ -1,4 +0,0 @@
-consumer_key        = 'odAzhp7tha2WCdZD6vwDaO4DM'
-consumer_secret     = 'ddLII73Igw6BzMcmArHfbcvJNrTimx1rPOexUkWN4MT077ZhiR'
-access_token        = '959699227117891584-ZccNj1BW7F5OYqhelGAc3jUNIoMG0Sq'
-access_token_secret = 'X03oGdkPZZdtMMtfAqYuUedyCAXlQYUzCgYbT2JqU1VHU'
